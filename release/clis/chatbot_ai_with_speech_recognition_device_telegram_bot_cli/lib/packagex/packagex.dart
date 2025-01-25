9c25a81e4988fabb03f5da960c325871f180b258
‎ <>
2025-01-26 05:56:36 +0700

    up

---
+++ release/clis/chatbot_ai_with_speech_recognition_device_telegram_bot_cli/lib/packagex/packagex.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: non_constant_identifier_names
+import 'dart:convert';
+
+/// AutoGenerateBy Packagex
+class PackagexProjectChatbotAiWithSpeechRecognitionDeviceTelegramBotCli {
+/// AutoGenerateBy Packagex
+  static bool isSame({
+    required String data
+  }) {
+    return [default_data_to_string, json.encode(default_data)].contains(data);
+  }
+/// AutoGenerateBy Packagex
+    static String get default_data_to_string {
+      return (JsonEncoder.withIndent(" " * 2).convert(default_data));
+    }
+/// AutoGenerateBy Packagex
+    static Map get default_data {
+return {
+  "name": "chatbot_ai_with_speech_recognition_device_telegram_bot_cli",
+  "description": "A sample command-line application.",
+  "version": "0.0.0",
+  "publish_to": "none",
+  "funding": []
+};
+    }
+
+}
