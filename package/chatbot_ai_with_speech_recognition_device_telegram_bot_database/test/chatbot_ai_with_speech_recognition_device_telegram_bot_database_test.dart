5853a8306d605df3034bcb0d0de26a5bd252b9ae
‎ <>
2025-01-26 05:22:44 +0700

    up

--- package/chatbot_ai_with_speech_recognition_device_telegram_bot_database/test/chatbot_ai_with_speech_recognition_device_telegram_bot_database_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:chatbot_ai_with_speech_recognition_device_telegram_bot_database/chatbot_ai_with_speech_recognition_device_telegram_bot_database.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
