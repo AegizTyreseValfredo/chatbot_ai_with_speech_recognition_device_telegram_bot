a24764707de202cb9c0b807a2c3c9b2a90b357d2
‎ <>
2025-01-26 05:28:23 +0700

    update

---
+++ release/clis/chatbot_ai_with_speech_recognition_device_telegram_bot_cli/bin/chatbot_ai_with_speech_recognition_device_telegram_bot_cli.dart
@@ -0,0 +1,5 @@
+import 'package:chatbot_ai_with_speech_recognition_device_telegram_bot_cli/chatbot_ai_with_speech_recognition_device_telegram_bot_cli.dart' as chatbot_ai_with_speech_recognition_device_telegram_bot_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${chatbot_ai_with_speech_recognition_device_telegram_bot_cli.calculate()}!');
+}
---
+++ release/clis/chatbot_ai_with_speech_recognition_device_telegram_bot_cli/test/chatbot_ai_with_speech_recognition_device_telegram_bot_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:chatbot_ai_with_speech_recognition_device_telegram_bot_cli/chatbot_ai_with_speech_recognition_device_telegram_bot_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
