4866059bd2d3cace63161fc8176accdbb41611c9
AegizTyreseValfredo <aegiztyresevlafredo@gmail.com>
2025-01-25 09:00:47 +0000

    up

---
+++ package/chatbot_ai_with_speech_recognition_device_telegram_bot_database/lib/database/core.dart
@@ -0,0 +1,8 @@
+
+/// Program Created By AegizTyreseValfredo From General Corporation & Global Corporation
+/// if you need some help or more update or more documentation 
+/// you should follow up General corporation & Global Corporation
+class ChatbotAiWithSpeechRecognitionDeviceTelegramBotDatabase {
+  ///
+  ChatbotAiWithSpeechRecognitionDeviceTelegramBotDatabase();
+}
